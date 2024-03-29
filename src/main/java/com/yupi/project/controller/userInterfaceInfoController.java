db49574e40c7432912b3369d1bf203e093016121
13425 <1342558165@qq.com>
2024-03-29 16:35:58 +0800

    add 用户-接口表增删改查

---
+++ src/main/java/com/yupi/project/controller/userInterfaceInfoController.java
@@ -0,0 +1,195 @@
+package com.yupi.project.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yupi.project.annotation.AuthCheck;
+import com.yupi.project.common.*;
+import com.yupi.project.constant.CommonConstant;
+import com.yupi.project.exception.BusinessException;
+import com.yupi.project.model.dto.userInterfaceInfo.UserInterfaceInfoAddRequest;
+import com.yupi.project.model.dto.userInterfaceInfo.UserInterfaceInfoQueryRequest;
+import com.yupi.project.model.dto.userInterfaceInfo.UserInterfaceInfoUpdateRequest;
+
+import com.yupi.project.model.entity.UserInterfaceInfo;
+import com.yupi.project.service.UserInterfaceInfoService;
+import com.yupi.project.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * 帖子接口
+ *
+ * @author yupi
+ */
+@RestController
+@RequestMapping("/userinterfaceInfo")
+@Slf4j
+public class userInterfaceInfoController {
+
+    @Resource
+    private UserInterfaceInfoService userinterfaceInfoService;
+
+    @Resource
+    private UserService userService;
+
+    // region 增删改查
+
+    /**
+     * 创建
+     *
+     * @param userInterfaceInfoAddRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/add")
+    public BaseResponse<Long> addUserInterfaceInfo(@RequestBody UserInterfaceInfoAddRequest userInterfaceInfoAddRequest, HttpServletRequest request) {
+        if (userInterfaceInfoAddRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        UserInterfaceInfo userinterfaceInfo = new UserInterfaceInfo();
+        BeanUtils.copyProperties(userInterfaceInfoAddRequest, userinterfaceInfo);
+        // 校验
+        userinterfaceInfoService.validUserInterfaceInfo(userinterfaceInfo, true);
+        com.yupi.project.model.entity.User loginUser = userService.getLoginUser(request);
+        userinterfaceInfo.setUserId(loginUser.getId());
+        boolean result = userinterfaceInfoService.save(userinterfaceInfo);
+        if (!result) {
+            throw new BusinessException(ErrorCode.OPERATION_ERROR);
+        }
+        long newUserInterfaceInfoId = userinterfaceInfo.getId();
+        return ResultUtils.success(newUserInterfaceInfoId);
+    }
+
+    /**
+     * 删除
+     *
+     * @param deleteRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/delete")
+    public BaseResponse<Boolean> deleteUserInterfaceInfo(@RequestBody DeleteRequest deleteRequest, HttpServletRequest request) {
+        if (deleteRequest == null || deleteRequest.getId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        com.yupi.project.model.entity.User user = userService.getLoginUser(request);
+        long id = deleteRequest.getId();
+        // 判断是否存在
+        UserInterfaceInfo oldUserInterfaceInfo = userinterfaceInfoService.getById(id);
+        if (oldUserInterfaceInfo == null) {
+            throw new BusinessException(ErrorCode.NOT_FOUND_ERROR);
+        }
+        // 仅本人或管理员可删除
+        if (!oldUserInterfaceInfo.getUserId().equals(user.getId()) && !userService.isAdmin(request)) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+        boolean b = userinterfaceInfoService.removeById(id);
+        return ResultUtils.success(b);
+    }
+
+    /**
+     * 更新
+     *
+     * @param UserInterfaceInfoUpdateRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/update")
+    public BaseResponse<Boolean> updateUserInterfaceInfo(@RequestBody UserInterfaceInfoUpdateRequest UserInterfaceInfoUpdateRequest,
+                                            HttpServletRequest request) {
+        if (UserInterfaceInfoUpdateRequest == null || UserInterfaceInfoUpdateRequest.getId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        UserInterfaceInfo userinterfaceInfo = new UserInterfaceInfo();
+        BeanUtils.copyProperties(UserInterfaceInfoUpdateRequest, userinterfaceInfo);
+        // 参数校验
+        userinterfaceInfoService.validUserInterfaceInfo(userinterfaceInfo, false);
+        com.yupi.project.model.entity.User user = userService.getLoginUser(request);
+        long id = UserInterfaceInfoUpdateRequest.getId();
+        // 判断是否存在
+        UserInterfaceInfo oldUserInterfaceInfo = userinterfaceInfoService.getById(id);
+        if (oldUserInterfaceInfo == null) {
+            throw new BusinessException(ErrorCode.NOT_FOUND_ERROR);
+        }
+        // 仅本人或管理员可修改
+        if (!oldUserInterfaceInfo.getUserId().equals(user.getId()) && !userService.isAdmin(request)) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+        boolean result = userinterfaceInfoService.updateById(userinterfaceInfo);
+        return ResultUtils.success(result);
+    }
+
+    /**
+     * 根据 id 获取
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/get")
+    public BaseResponse<UserInterfaceInfo> getUserInterfaceInfoById(long id) {
+        if (id <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        UserInterfaceInfo userinterfaceInfo = userinterfaceInfoService.getById(id);
+        return ResultUtils.success(userinterfaceInfo);
+    }
+
+    /**
+     * 获取列表（仅管理员可使用）
+     *
+     * @param UserInterfaceInfoQueryRequest
+     * @return
+     */
+    @AuthCheck(mustRole = "admin")
+    @GetMapping("/list")
+    public BaseResponse<List<UserInterfaceInfo>> listUserInterfaceInfo(UserInterfaceInfoQueryRequest UserInterfaceInfoQueryRequest) {
+        UserInterfaceInfo UserInterfaceInfoQuery = new UserInterfaceInfo();
+        if (UserInterfaceInfoQueryRequest != null) {
+            BeanUtils.copyProperties(UserInterfaceInfoQueryRequest, UserInterfaceInfoQuery);
+        }
+        QueryWrapper<UserInterfaceInfo> queryWrapper = new QueryWrapper<>(UserInterfaceInfoQuery);
+        List<UserInterfaceInfo> UserInterfaceInfoList = userinterfaceInfoService.list(queryWrapper);
+        return ResultUtils.success(UserInterfaceInfoList);
+    }
+
+    /**
+     * 分页获取列表
+     *
+     * @param UserInterfaceInfoQueryRequest
+     * @param request
+     * @return
+     */
+    @GetMapping("/list/page")
+    public BaseResponse<Page<UserInterfaceInfo>> listUserInterfaceInfoByPage(UserInterfaceInfoQueryRequest UserInterfaceInfoQueryRequest, HttpServletRequest request) {
+        if (UserInterfaceInfoQueryRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        UserInterfaceInfo UserInterfaceInfoQuery = new UserInterfaceInfo();
+        BeanUtils.copyProperties(UserInterfaceInfoQueryRequest, UserInterfaceInfoQuery);
+        long current = UserInterfaceInfoQueryRequest.getCurrent();
+        long size = UserInterfaceInfoQueryRequest.getPageSize();
+        String sortField = UserInterfaceInfoQueryRequest.getSortField();
+        String sortOrder = UserInterfaceInfoQueryRequest.getSortOrder();
+        // 限制爬虫
+        if (size > 50) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        QueryWrapper<UserInterfaceInfo> queryWrapper = new QueryWrapper<>(UserInterfaceInfoQuery);
+        queryWrapper.orderBy(StringUtils.isNotBlank(sortField),
+                sortOrder.equals(CommonConstant.SORT_ORDER_ASC), sortField);
+        Page<UserInterfaceInfo> UserInterfaceInfoPage = userinterfaceInfoService.page(new Page<>(current, size), queryWrapper);
+        return ResultUtils.success(UserInterfaceInfoPage);
+    }
+
+    // endregion
+
+
+
+
+}
